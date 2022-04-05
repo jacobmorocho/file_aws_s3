3e504b7066524d63b957b6f27ce103216b982458
Elías Morocho <elias.morocho@fusertech.com.pe>
2022-04-05 17:54:22 -0500

    todas

--- src/app.js
+++ src/app.js
@@ -11,6 +11,5 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(BodyParser.json());
 app.use(cors());
-app.use("/api/aws", DocumentsRoutes);
-
+app.use("/api/aws",  DocumentsRoutes);
 module.exports = app;
--- src/controller/aws.js
+++ src/controller/aws.js
@@ -1,55 +1,157 @@
 const { exec } = require("child_process");
+var jsv = require('json-validator');
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
-const AwsController = () => {
-    const clone = (req, res) => {
-        exec("aws s3 cp s3://dev-aws-backet/tienda01/ s3://dev-aws-backet/tienda02 --recursive ", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
-            }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-        res.json({ title: "List" })
+const ValidateClone = (body, callback) => {
+    var schema = {
+        backet: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [5, 50]
+        },
+        domainorg: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [3, 50]
+        },
+        domaindist: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [3, 50]
+        }
     }
-    const add = (req, res) => {
+    jsv.validate(body, schema, callback);
+}
+const Validate = (body, callback) => {
+    var schema = {
+        backet: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [5, 50]
+        },
+        domain: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [3, 50]
+        },
+        key: {
+            required: true,
+            trim: true,
+            type: "string",
+            isLength: [1, 50]
+        }
+    }
+    jsv.validate(body, schema, callback);
+}
+const IsBucket = async (BUCKET_NAME) => {
+    try {
+        const data = await s3.headBucket({ Bucket: BUCKET_NAME }).promise()
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+const commandCli = (command) => {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+const pathKey = (args) => {
+    let key = args.join("/")
+    const search = '//'
+    const replacer = new RegExp(search, 'g')
+    key = key.replace(replacer, '/');
+    return key;
+}
+const AwsController = () => {
+    const clone = async (req, res) => {
+        ValidateClone(req.body, (err, messages) => {
+            if (messages && Object.keys(messages).length !== 0) {
+                res.json(messages);
+            } else {
+                IsBucket(req.body.backet).then((r) => {
+                    if (r) {
+                        let command = `aws s3 cp s3://${req.body.backet}/${req.body.domainorg}/ s3://${req.body.backet}/${req.body.domaindist} --recursive `;
+                        commandCli(command);
+                        res.json({ status: true, messages: "En ejecucion" });
+                    }
+                    else {
+                        res.json({ status: false, message: "No Existe backet" });
+                    }
+                }).catch((err) => {
+                    res.json(err);
+                })
+            }
+        });
+    }
+    const add = async (req, res) => {
         if (!req.files || Object.keys(req.files).length === 0) {
             return res.status(400).send('No files were uploaded.');
         }
-        const params = {
-            Bucket: "dev-aws-backet",
-            Key: req.files.file.name,
-            Body: JSON.stringify(req.files.file, null, 2)
-        };
-        console.log( s3);
-        s3.upload(params, function (err, data) {
-            if (err) {
-                throw err;
-            }
-            res.json(data);
-        });
+        const body = JSON.parse(JSON.stringify(req.body));
+        Validate(body, (err, messages) => {
+            if (messages && Object.keys(messages).length !== 0) {
+                res.json(errors);
+            } else {
+                IsBucket(req.body.backet).then((r) => {
+                    if (r) {
+                        let key = pathKey([req.body.domain, req.body.key, req.files.file.name]);
+                        const params = {
+                            Bucket: req.body.backet,
+                            Key: key,
+                            Body: JSON.stringify(req.files.file, null, 2)
+                        };
+                        s3.upload(params, function (err, data) {
+                            if (err) {
+                                res.json(err);
+                            }
+                            res.json(data);
+                        });
+                    } else {
+                        res.json({ status: false, message: "No Existe backet" });
+                    }
 
+                }).catch((err) => {
+                    res.json(err);
+                })
+            }
+        })
     }
-    const deleted = (req, res) => {
-        exec("aws s3 rm s3://dev-aws-backet/tienda02/ --recursive", (error, stdout, stderr) => {
-            if (error) {
-                console.log(`error: ${error.message}`);
-                return;
+    const deleted = async (req, res) => {
+        Validate(req.body, (err, messages) => {
+            if (messages && Object.keys(messages).length !== 0) {
+                res.json(messages);
+            } else {
+                IsBucket(req.body.backet).then((r) => {
+                    if (r) {
+                        let key = pathKey([req.body.domain, req.body.key]);
+                        let command = `aws s3 rm s3://${req.body.backet}/${key} --recursive`;
+                        commandCli(command);
+                        res.json({ status: true, message: "En ejecucion" });
+                    } else {
+                        res.json({ status: false, message: "No Existe backet" });
+                    }
+
+                }).catch((err) => {
+                    res.json(err);
+                })
             }
-            if (stderr) {
-                console.log(`stderr: ${stderr}`);
-                return;
-            }
-            console.log(`stdout: ${stdout}`);
-        });
-        res.json({ title: "delete" })
+        })
     }
     return {
         clone: clone,
